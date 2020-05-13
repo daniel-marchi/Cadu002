d820e728605f263af1eb296e404c26c37eb1dc61
Daniel Marchi <marchimail@gmail.com>
2020-05-13 09:34:55 -0300

    meu primeiro comit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cadu002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
